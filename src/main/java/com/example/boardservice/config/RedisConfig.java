d41daef5cd4dbcb07e8654fafb26650c09668a2f
jw <wjd10577@naver.com>
2026-02-09 09:35:02 +0900

    add RedisConfig.java

--- src/main/java/com/example/boardservice/config/RedisConfig.java
+++ src/main/java/com/example/boardservice/config/RedisConfig.java
@@ -1,22 +1,21 @@
-// package com.example.boardservice.config;
+package com.example.boardservice.config;
 
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-// import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
-// @Configuration
-// public class RedisConfig {
-//     @Value("${spring.data.redis.host}")
-//     private String host;
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
 
-//     @Value("${spring.data.redis.port}")
-//     private int port;
+    @Value("${spring.data.redis.port}")
+    private int port;
 
-//     @Bean
-//     public LettuceConnectionFactory redisConnectionFactory() {
-//         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
-//     }
-
-// }
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+}

4386a8e6ee6c9fa83de123363349be7da74b748b
jw <wjd10577@naver.com>
2026-02-18 16:02:12 +0900

    feat: redis off

--- src/main/java/com/example/boardservice/config/RedisConfig.java
+++ src/main/java/com/example/boardservice/config/RedisConfig.java
@@ -1,12 +1,17 @@
 package com.example.boardservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
+@ConditionalOnProperty(
+    name = "cache.redis.enable",
+    havingValue = "true"
+)
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
